aa41034460d94f245796dca2a8095a6536e9b2a3
nmastroianni <suancho@gmail.com>
2021-06-20 10:11:30 -0400

    temporarily disable sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
+    // `gatsby-plugin-sitemap`,
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
